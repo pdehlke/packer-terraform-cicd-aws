ad00961a59b7a40cdcf921044795d91e20fcca4a
Pete Ehlke <pete.ehlke@aboveproperty.com>
2021-05-20 12:29:49 -0700

    sigh. terraform

--- terraform.tf
+++ terraform.tf
@@ -9,21 +9,21 @@ variable "vpc_dmz_cidr" {
   type = "string"
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "${var.vpc_main_cidr}"
-# }
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_main_cidr}"
+}
 
-# output "main_vpc_id" {
-#   value = "${aws_vpc.main.id}"
-# }
+output "main_vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
 
-# resource "aws_vpc" "dmz" {
-#   cidr_block = "${var.vpc_dmz_cidr}"
-# }
+resource "aws_vpc" "dmz" {
+  cidr_block = "${var.vpc_dmz_cidr}"
+}
 
-# output "dmz_vpc_id" {
-#   value = "${aws_vpc.dmz.id}"
-# }
+output "dmz_vpc_id" {
+  value = "${aws_vpc.dmz.id}"
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
