4187a9a4a49549ca8dd93f67fc04625c5bb7ceed
Pete Ehlke <pete.ehlke@aboveproperty.com>
2021-05-24 04:04:21 -0700

    Accept inspec license

--- aws-security/controls/example.rb
+++ aws-security/controls/example.rb
@@ -3,7 +3,7 @@ content = inspec.profile.file("output.json")
 params = JSON.parse(content)
 
 vpc_id = params['main_vpc_id']['value']
-dmz_vpc_id = params['dmz_vpc_id']['value']
+alb = params['alb_arn']['value']
 
 describe aws_vpc(vpc_id) do
   its('state') { should eq 'available' }
@@ -12,7 +12,7 @@ describe aws_vpc(vpc_id) do
   # its('cidr_block') { should eq '172.18.0.0/16' }
 end
 
-describe aws_vpc(dmz_vpc_id) do
-  its('state') { should eq 'available' }
+describe aws_alb(alb) do
+  it { should exist }
   # its('cidr_block') { should eq '172.19.0.0/16' }
 end
--- terraform.tf
+++ terraform.tf
@@ -241,6 +241,14 @@ data "aws_ami" "centos" {
   }
 }
 
+output "main_vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "alb_arn" {
+  value = module.alb.lb_arn
+}
+
 output "alb_dns_name" {
   value = module.alb.lb_dns_name
 }
