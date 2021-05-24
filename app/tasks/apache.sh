f3ef83eb68631597a0f5d94512fa2e38de51204c
Pete Ehlke <pete.ehlke@aboveproperty.com>
2021-05-24 09:31:32 -0700

    app builder waits for apt locks

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -e
 
-echo '---- install Apache'
+apt_wait () {
+  echo '---- waiting for apt locks'
+  while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  if [ -f /var/log/unattended-upgrades/unattended-upgrades.log ]; then
+    while sudo fuser /var/log/unattended-upgrades/unattended-upgrades.log >/dev/null 2>&1 ; do
+      sleep 1
+    done
+  fi
+}
 
+echo '---- update apt'
+apt_wait
 DEBIAN_FRONTEND=noninteractive apt-get -y update
+apt_wait
+echo '---- install apache'
 DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
 
 cat > /var/www/html/index.html <<HERE
